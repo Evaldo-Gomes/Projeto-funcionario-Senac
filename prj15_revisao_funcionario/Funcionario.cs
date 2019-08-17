7ce633ed9b8cd607a2c6e4477e65c42345a281df
Evaldo Gomes <evaldo_gomes@hotmail.com>
2019-08-17 12:06:54 -0300

    subindo o projeto

---
+++ prj15_revisao_funcionario/Funcionario.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace prj15_revisao_funcionario
+{
+    class Funcionario
+    {
+        public string Nome { get; private set; }
+        public string Cargo { get; private set; }
+        public decimal Salario { get; private set; }
+        public double Desconto { get; private set; }
+
+        public Funcionario(string nome, string cargo, decimal salario, double desconto)
+        {
+            this.Nome = nome;
+            this.Cargo = cargo;
+            this.Salario = salario;
+            this.Desconto = desconto;
+        }
+
+        public decimal SaLiquido()
+        {
+            return Salario -= (Salario *(decimal) Desconto) / 100;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} você possui o cargo de {1} com o desconto de {2}% " +
+                "receberá o salario de R$ {3}", Nome, Cargo,
+                Desconto.ToString("f2", CultureInfo.InvariantCulture), SaLiquido().ToString("f2"));
+        }
+
+    }
+}
---
+++ prj15_revisao_funcionario/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prj15_revisao_funcionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe o nome do Funcionario: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Informe o cargo do funcionario: ");
+            string cargo = Console.ReadLine();
+
+            Console.Write("Informe o salário do funcionario: ");
+            decimal salario = decimal.Parse(Console.ReadLine());
+
+            Console.Write("Informe o total percentual de desconto: ");
+            double desconto = double.Parse(Console.ReadLine());
+
+            Funcionario func = new Funcionario(nome, cargo, salario, desconto);
+
+            Console.WriteLine();
+            Console.WriteLine(func);
+            
+        }
+    }
+}
